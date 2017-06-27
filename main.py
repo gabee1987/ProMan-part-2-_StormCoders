ba62d473b36813b9607cab05c8e6a471e9e80402
gabee1987 <gabee1987@gmail.com>
2017-06-27 14:03:01 +0200

    Created boards save feature(js,python)

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, session, url_for, flash, jsonify
+import json
+import querys
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
+@app.route('/index')
+def index():
     return render_template('index.html')
+
+
+@app.route('/saveboard', methods=['POST'])
+def saveboard():
+    json_board_data = request.json['JSONBoard']
+    board_data = json.loads(json_board_data)
+    query = querys.save_board
+    data_to_query = (board_data['boardObj']['title'], board_data['boardObj']['state'])
+    # result = handle_database(query, values)
+    print(json_board_data)
+    print(board_data)
+    print(board_data['boardObj']['title'])
+    print(board_data['boardObj']['state'])
+    print(data_to_query)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,14 @@
 function main() {
     $('#addBoardButton').on('click', function(){
-        setBoardsToStorage();
+        var boardTitle = $('#addBoardTitle').val();
+        var boardState = 'active';
+        //if input is empty alert the user
+        if($('#addBoardTitle').val() == '') {
+            $('#alert').html('<strong>Warning!</strong> You left the title empty');
+            $('#alert').fadeIn().delay(1000).fadeOut();
+            return false;
+        };
+        saveBoard(boardTitle, boardState);
     });
     $('.view-board').on('click', function(){
         getCards();
